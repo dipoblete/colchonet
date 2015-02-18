596d3281bfc4b2808214f1dc887d7f75672bc814
Diego Poblete <dpoblete@bionexo.com>
2015-02-18 13:19:53 -0200

    pagina 121

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,5 +1,9 @@
 class Room < ActiveRecord::Base
-	def complete_name
+	 
+   validates_presence_of :title, :location, :description
+   validates_length_of :description, minimum: 10, allow_blank: false
+
+  def complete_name
 	  "#{title}, #{location}"
 	end	
 end
