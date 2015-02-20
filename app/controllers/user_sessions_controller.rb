1e506cba6fcdc6eb9477fe8ef88fc11e7b7c5ee9
Diego Poblete <dpoblete@bionexo.com>
2015-02-20 16:40:39 -0200

    pagina 163

---
+++ app/controllers/user_sessions_controller.rb
@@ -0,0 +1,14 @@
+class UserSessionsController < ApplicationController
+  def new
+    @user_session = UserSession.new
+  end
+
+  def create
+    # not yet :)
+  end
+
+  def destroy
+    #not yet :)
+  end
+end
+
---
+++ app/models/user_session.rb
@@ -0,0 +1,8 @@
+class UserSession
+  include ActiveModel::Model
+
+  attr_accessor :email, :password
+  validates_presence_of :email, :password
+
+end
+
