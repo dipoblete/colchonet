8c46b64f5d75fe8100c87d499401aa79c4707525
Diego Poblete <dpoblete@bionexo.com>
2015-02-20 08:28:38 -0200

    finalizando o capitulo 8

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
  LOCALES = /en|pt\-BR/
 
- scope "(:locale)", locale: LOCALES  do 
+ scope "(:locale)", locale: LOCALES  do
   resources :rooms
   resources :users
  end
+
+ resource :confirmation, only: [:show]
 
   get '/:locale' => 'home#index', locale: LOCALES
   root 'home#index'
