ff7d7a33dee1fa2067733ecf945a7a7c4ca9e478
Diego Poblete <poblete@outlook.com>
2015-02-19 22:27:43 -0200

    pagina 146 enviando email

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,16 +6,19 @@ class UsersController <  ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+
+      Signup.confirm_email(@user).deliver
+
       redirect_to @user,
           notice: 'cadastro realizado com sucesso!'
     else
       render action: :new
-    end  
+    end
   end
 
   def show
     @user = User.find(params[:id])
-  end 
+  end
 
   def edit
     @user = User.find(params[:id])
