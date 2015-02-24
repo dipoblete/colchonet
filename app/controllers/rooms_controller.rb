ea6d3200aa83f8f51767b4e6d640fe66fb7a591c
Diego Poblete <dpoblete@bionexo.com>
2015-02-24 08:49:31 -0300

    finalizando 10.2 pagina 196

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,4 +1,4 @@
-class RoomsController < ApplicationControllee
+class RoomsController < ApplicationController
   before_action :require_authentication,
         only: [:new, :edit, :create, :update, :destroy]
 
@@ -19,45 +19,29 @@ class RoomsController < ApplicationControllee
   end
 
   def create
-    @room = Room.new(room_params)
+    @room = Room.new(params[:room])
 
-    respond_to do |format|
       if @room.save
-        format.html { redirect_to @room, notice: 'Room was successfully created.' }
-        format.json { render :show, status: :created, location: @room }
+         redirect_to @room, notice: t('flash.notice.room_created')
       else
-        format.html { render :new }
-        format.json { render json: @room.errors, status: :unprocessable_entity }
+        render action: "new"
       end
-    end
   end
 
   def update
-    respond_to do |format|
-      if @room.update(room_params)
-        format.html { redirect_to @room, notice: 'Room was successfully updated.' }
-        format.json { render :show, status: :ok, location: @room }
+    @room = Room.find(params[:id])
+
+    if @room.update(params[:room])
+        redirect_to @room, notice: t('flash.notice.room_updated.')
       else
-        format.html { render :edit }
-        format.json { render json: @room.errors, status: :unprocessable_entity }
-      end
+        render action: "edit"
     end
   end
 
   def destroy
+    @room = Room.find(params[:id])
     @room.destroy
-    respond_to do |format|
-      format.html { redirect_to rooms_url, notice: 'Room was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+
+    redirect_to rooms_url
   end
-
-  private
-    def set_room
-      @room = Room.find(params[:id])
-    end
-
-    def room_params
-      params.require(:room).permit(:title, :location, :description)
-    end
 end
